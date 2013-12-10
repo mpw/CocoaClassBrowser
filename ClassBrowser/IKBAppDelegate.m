451204eb042bb45268ef41046f2fb4d9f3b6d7a6
Graham Lee <graham@bignerdranch.com>
2013-12-10 15:13:08 +0000

    Implementing the tested action method gives us some reparation for the broken selection-change behaviour.

--- ClassBrowser/IKBAppDelegate.m
+++ ClassBrowser/IKBAppDelegate.m
@@ -34,6 +34,16 @@
     self.browserSource = [[IKBClassBrowserSource alloc] initWithClassList:classList];
     self.classBrowser.delegate = self.browserSource;
     [self.classBrowser reloadColumn:0];
+    [self.classBrowser setTarget:self];
+    [self.classBrowser setAction:@selector(browserSelectionDidChange:)];
+}
+
+// this behaviour should be encapsulated in a window controller
+- (IBAction)browserSelectionDidChange:(NSBrowser *)sender
+{
+    NSInteger column = [sender selectedColumn];
+    NSInteger row = [sender selectedRowInColumn:column];
+    [self.browserSource browser:sender didSelectRow:row inColumn:column];
 }
 
 @end
