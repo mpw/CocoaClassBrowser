f6745a6ff9573e15e98bb55cfa68ca100497aa21
Graham Lee <graham@bignerdranch.com>
2014-01-09 13:25:30 +0000

    Make sure the code is actually run.

--- ClassBrowserTests/CompileAndRunCodeCommandHandlerTests.m
+++ ClassBrowserTests/CompileAndRunCodeCommandHandlerTests.m
@@ -7,10 +7,15 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
+#import "IKBCodeRunner.h"
 #import "IKBCommandHandler.h"
 #import "IKBCompileAndRunCodeCommand.h"
 
 @interface IKBCompileAndRunCodeCommandHandler : NSObject <IKBCommandHandler>
+
+@property (nonatomic, strong) IKBCodeRunner *codeRunner;
 
 @end
 
@@ -21,9 +26,9 @@
     return [command isKindOfClass:[IKBCompileAndRunCodeCommand class]];
 }
 
-- (void)executeCommand:(id<IKBCommand>)command
+- (void)executeCommand:(IKBCompileAndRunCodeCommand *)command
 {
-    
+    [self.codeRunner doIt:command.source completion:command.completion];
 }
 
 @end
@@ -31,6 +36,7 @@
 @interface CompileAndRunCodeCommandHandlerTests : XCTestCase
 {
     IKBCompileAndRunCodeCommandHandler *_handler;
+    IKBCompileAndRunCodeCommand *_command;
 }
 
 @end
@@ -40,17 +46,30 @@
 - (void)setUp
 {
     _handler = [IKBCompileAndRunCodeCommandHandler new];
+    _command = [IKBCompileAndRunCodeCommand new];
+    _command.source = @"Test Source";
+    _command.completion = ^(id result, NSString *transcript, NSError *error) {
+        
+    };
 }
 
 - (void)testTheHandlerWillAcceptCompileAndRunSourceCommands
 {
-    IKBCompileAndRunCodeCommand *command = [IKBCompileAndRunCodeCommand new];
-    XCTAssertTrue([_handler canHandleCommand:command]);
+    XCTAssertTrue([_handler canHandleCommand:_command]);
 }
 
 - (void)testHandlerDoesNotAcceptAnyOldCommand
 {
     XCTAssertFalse([_handler canHandleCommand:(id)@"Not the expected command"]);
+}
+
+- (void)testExecutionInvolvesRunningTheCode
+{
+    id mockRunner = [OCMockObject mockForClass:[IKBCodeRunner class]];
+    [[mockRunner expect] doIt:_command.source completion:_command.completion];
+    _handler.codeRunner = mockRunner;
+    [_handler executeCommand:_command];
+    [mockRunner verify];
 }
 
 @end
