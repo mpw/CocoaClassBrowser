d045727ffca0c6f558fc91c278d47eb4966af7a0
Graham Lee <leeg@fb.com>
2015-07-13 19:01:52 +0100

    Quick builder for canonical method names so they can be rendered in the browser.

--- ClassBrowser/Modeling Objective-C Concepts/IKBObjectiveCMethod.h
+++ ClassBrowser/Modeling Objective-C Concepts/IKBObjectiveCMethod.h
@@ -8,5 +8,10 @@
 @property (nonatomic, copy) NSString *declaration;
 @property (nonatomic, copy) NSString *body;
 @property (nonatomic, copy) NSString *className;
+/**
+ * The canonical name is that which would be presented in the browser
+ * were this method attached to the runtime, which it isn't, necessarily.
+ */
+@property (nonatomic, readonly) NSString *canonicalName;
 
 @end
