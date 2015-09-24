0a9b67e31a90cfd52cecfd0f358f432139534832
Graham Lee <graham@bignerdranch.com>
2015-09-24 14:22:11 +0100

    Create the source repository on launch.

--- ClassBrowser/IKBAppDelegate.h
+++ ClassBrowser/IKBAppDelegate.h
@@ -3,8 +3,11 @@
 #import <Cocoa/Cocoa.h>
 
 @class IKBCommandBus;
+@class IKBSourceRepository;
+
 @interface IKBAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (nonatomic, strong) IKBCommandBus *commandBus;
+@property (nonatomic, strong) IKBSourceRepository *repository;
 
 @end
